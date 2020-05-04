e8e5f2b48bbe6ab42f4347dd39871a08ab271385
Reivnaj <pelletierjanvier@gmail.com>
2020-05-04 17:31:29 +0200

    url caché

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+
   end
   def contact
-    
+
+  end
+  def welcome
+    @first_name = params['first_name']
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'team', to: 'static_pages#home'
   get 'contact', to: 'static_pages#contact'
+  get 'welcome/:first_name', to: 'static_pages#welcome'
 end
