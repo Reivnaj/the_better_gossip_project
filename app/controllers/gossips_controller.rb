457a8cdd932aebab90816eacf199c7768e57d34e
Reivnaj <pelletierjanvier@gmail.com>
2020-05-05 18:25:30 +0200

    create form_tag for gossips

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -4,6 +4,16 @@ class GossipsController < ApplicationController
   end
 
   def create
+    user = User.find_by(first_name: "anonymous")
+    @gossip = Gossip.new(user: user, title: params['title'], content: params['content']) # avec xxx qui sont les données obtenues à partir du formulaire
+
+    if @gossip.save # essaie de sauvegarder en base @gossip
+      # si ça marche, il redirige vers la page d'index du site
+      redirect_to "/"
+    else
+      # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      render :action => "new"
+    end
 
   end
 
