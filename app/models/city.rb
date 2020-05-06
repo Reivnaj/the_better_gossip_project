f5e4cbdaef5fb4ebb659b00bf4627ebe8eb59de9
Reivnaj <pelletierjanvier@gmail.com>
2020-05-06 16:47:37 +0200

    create City page whitch show all gossips from the city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,10 +1,10 @@
 class City < ApplicationRecord
 	has_many :users
-	
+	has_many :gossips, through: :users 
 	# Nom obligatoire
 	validates :name, presence: true
 	# Zip code unique et au format français
-	validates :zip_code, 
+	validates :zip_code,
 		presence: true,
 		uniqueness: true,
 		format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "please enter a valid french zip code"}
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :gossips, only: [:new, :create, :show, :index]
   resources :users, only: [:show]
+  resources :cities, only: [:show]
   get 'team', to: 'static_pages#home'
   get 'contact', to: 'static_pages#contact'
   get 'welcome/:first_name', to: 'static_pages#welcome'
