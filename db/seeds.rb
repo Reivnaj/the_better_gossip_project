adbaf4d8e67631abe10052234c4632a63ef3ef89
sofiachakir <sofiachakir@gmail.com>
2020-05-07 20:39:46 +0200

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ require 'faker'
 Faker::Config.locale = 'fr'
 
 # Détruire la base actuelle
+Like.destroy_all
+Comment.destroy_all
 GossipTagLink.destroy_all
 Gossip.destroy_all
 Tag.destroy_all
@@ -24,37 +26,64 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-# Création de 10 villes et de 10 utilisateurs associés
-10.times do
-	City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
-	User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: true), email: Faker::Internet.free_email, age: rand(20..50), city: City.all.sample)
+# Création de 10 villes
+10.times do |i|
+	City.create(name: Faker::Address.unique.city, zip_code: Faker::Address.unique.zip_code)
+	puts "#{i+1} ville créée"
 end
+puts "-"*50
 
+# Création de 10 utilisateurs
+10.times do |i|
+	User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: true), email: Faker::Internet.free_email, age: rand(20..50), city: City.all.sample)
+	puts "#{i+1} utilisateur créé"
+end
+puts "-"*50
+
+# Création  de l'anonymous
 anonymous = User.create(first_name: 'anonymous', last_name: 'anonymous', description: 'I am anonymous', email: 'anonymous@anonymous.com', age: 70, city: City.first)
+puts "utilisateur anonyme créé"
+puts "-"*50
 
 # Création de 10 gossips
-20.times do
-	Gossip.create(title: Faker::Lorem.sentence(word_count: 3, supplemental: false), content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: true), user_id: User.ids.sample)
+20.times do |i|
+	Gossip.create(title: Faker::Book.unique.genre[0..12], content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: true), user_id: User.ids.sample)
+puts "#{i+1} potin créé"
 end
+puts "-"*50
+
 # Création de 10 tags
-10.times do
-	Tag.create(title: "##{Faker::Lorem.word}")
+10.times do |i|
+	Tag.create(title: "##{Faker::Color.unique.color_name.gsub(/\s+/, "_")}")
+	puts "#{i+1} tag créé"
 end
+puts "-"*50
+
 # Ajout des tags aux gossips (minimum un tag par gossip)
-Gossip.all.each do |g|
+Gossip.all.each_with_index do |g, i|
 	n = rand(1..3)
-	n.times do
-		GossipTagLink.create(gossip: g, tag: Tag.all.sample)
+	n.times do |j|
+		GossipTagLink.create(gossip: g, tag: Tag.find(n+j+1))
 	end
+	puts "#{n} tags ajoutés au potin #{i+1}"
 end
+puts "-"*50
+
 # Création de fakes PM
-10.times do
-	PrivateMessage.create(content: Faker::Hipster.paragraph_by_chars, sender: User.all.sample)
+10.times do |i|
+	PrivateMessage.create(content: Faker::Hipster.paragraph_by_chars, sender: User.find(i+1))
+	puts "#{i+1} message privé créé"
 end
+puts "-"*50
+
 # Ajout des destinataires aux privates messages (minimum un destinataire par message)
-PrivateMessage.all.each do |pm|
+PrivateMessage.all.each_with_index do |pm, i|
 	n = rand(1..3)
 	n.times do
-		PrivateMessageRecipient.create(private_message: pm, recipient: User.all.sample)
+		PrivateMessageRecipient.create(private_message: pm, recipient: User.where.not(id: pm.sender_id).sample)
 	end
+	puts "#{n} destinataires ajoutés au message privé #{i+1}"
 end
+puts "-"*50
+
+puts "Seed terminé"
