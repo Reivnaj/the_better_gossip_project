afc4c3b00d20a8ced481d4b3f1d2dd8b1330d805
sofiachakir <sofiachakir@gmail.com>
2020-05-08 15:33:08 +0200

    Set root to gossip#index and correcting City choice when sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-
-    @all_cities = City.all
     @user = User.new(user_params) # avec xxx qui sont les données obtenues à partir du formulaire
-    @user.city = City.first 
     if @user.save # essaie de sauvegarder en base @user
       # si ça marche, il message "Bravo ! Tu es inscri ! " redirige vers la page d'index du site
-      flash[:success] = "Bravo ! Tu es inscri !"
+      flash[:success] = "Bravo ! Tu es inscrit !"
       log_in(@user)
       redirect_to gossips_path
     else
@@ -28,7 +25,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :description, :age, :city, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :description, :age, :city_id, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :gossips do
     resources :comments
-      resources :likes
+    resources :likes
   end
   resources :users, only: [:show, :new, :create]
   resources :cities, only: [:show]
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get 'team', to: 'static_pages#home'
   get 'contact', to: 'static_pages#contact'
   get 'welcome/:first_name', to: 'static_pages#welcome'
+  root to: 'gossips#index'
 end
